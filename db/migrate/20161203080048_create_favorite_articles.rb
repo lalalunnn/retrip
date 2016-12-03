b265ee0ddb33e161e46b072fcbcbbc0f29f5672f
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-12-03 17:04:54 +0900

    favorite_articles columm migration
    rake db:migrate

--- db/migrate/20161203080048_create_favorite_articles.rb
+++ db/migrate/20161203080048_create_favorite_articles.rb
@@ -1,6 +1,8 @@
 class CreateFavoriteArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :favorite_articles do |t|
+      t.references :user, foreign_key: true
+      t.references :article, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203074246) do
+ActiveRecord::Schema.define(version: 20161203080048) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",      limit: 65535, null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20161203074246) do
     t.string   "image"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "favorite_articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_favorite_articles_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_favorite_articles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 20161203074246) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "favorite_articles", "articles"
+  add_foreign_key "favorite_articles", "users"
 end
