ff77b8466e87c4ced5168fb8269844e8eb7a2e79
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-12-03 16:59:17 +0900

    articles columm migration
    rake db:migrate

--- db/migrate/20161203074246_create_articles.rb
+++ db/migrate/20161203074246_create_articles.rb
@@ -1,6 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
+      t.text    :title, null: false
+      t.text    :body, null: false
+      t.string  :image
 
       t.timestamps
     end
