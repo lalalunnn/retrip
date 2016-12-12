2578f265813b943ecc0d0153d5179503cefee4b0
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-12-12 19:42:07 +0900

    Add post function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
@@ -7,5 +8,21 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(create_params)
+    if @article.save
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+    def create_params
+      params.permit( :image, :body, :title)
+      # params.require(:article).permit( :image, :body, :title)
+    end
 end
