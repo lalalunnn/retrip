f2cc14bac9970042c698f22a86b3a115cb7e4707
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-12-13 22:13:43 +0900

    fix controllers/articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [ :show]
+
   def index
-    @articles = Article.all
+    # 注目のまとめ
+    @articles = Article.all.page(params[:page]).per(10).order("created_at DESC")
+
+    # indexMain(3画像)
+    @sliders = Article.order("RAND()").limit(3)
   end
 
   def show
-    @article = Article.find(1)
   end
 
   def new
@@ -12,16 +17,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(create_params)
+    @article = Article.new(article_params)
     if @article.save
-      redirect_to :back
+      redirect_to root_path
     else
-      redirect_to :back
+      redirect_to new_article_path
     end
   end
 
   private
-    def create_params
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
       params.permit( :image, :body, :title)
       # params.require(:article).permit( :image, :body, :title)
     end
