e49fb044ebaa818a0cb2aa7d2ce3348c20172261
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-12-23 21:27:50 +0900

    Fix action in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    # ログイン中のユーザーが登録したお気に入り一覧
+    @user = User.find(current_user.id)
+    @articles = Article.where(user_id: current_user.id)
   end
 end
