eb48401f14dfea658db83200d35e52a344ed1f52
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-12-26 14:46:53 +0900

    add locations to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :locations
   devise_scope :user do
     get 'users', to: 'devise/registrations#new'
   end
@@ -8,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :articles, only: [:index, :new, :create, :show]
+  resources :locations, only: [:index, :new, :create, :show, :edit, :destroy]
 
   post 'articles/:article_id' => 'favorite_articles#create', as: 'like'
   delete 'articles/:article_id' => 'favorite_articles#destroy', as: 'unlike'
