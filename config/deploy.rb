6e572e70640b9ffb235635c1e9630bee8ec410bb
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2017-01-18 18:55:26 +0900

    fix config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,65 @@
 # config valid only for current version of Capistrano
 lock '3.7.1'
 
-set :application, 'rails-capistrano-test'
+# デプロイするアプリケーション名に変更してください
+set :application, 'retrip'
+
+# cloneするgitのレポジトリに変更してください
 set :repo_url, 'git@github.com:lalalunnn/retrip.git'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'master'
 
-set :rbenv_type, :user
+# deploy先のディレクトリに変更してください
+set :deploy_to, '/var/www/retrip'
+
+# シンボリックリンクをはるファイル。(※後述)
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+
+# シンボリックリンクをはるフォルダ。(※後述)
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
+
+# rubyのバージョン（ruby -vコマンドで確認し、ご自身が使用しているバージョンを指定してください。）
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['aws_rsa.pem']
+#出力するログのレベル。
+set :log_level, :debug
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-
-after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
   end
 end
