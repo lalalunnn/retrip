521b00c523caa67a7d6885de17fee75e9bc78946
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2017-01-18 18:55:39 +0900

    fix config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@
 server '52.197.29.76', user: 'nozawa', roles: %w{app db web}
 
 #デプロイするサーバにsshログインする鍵の情報を記述
-# set :ssh_options, keys: '~/.ssh/retrip_key_rsa'
+set :ssh_options, keys: '~/.ssh/retrip_key_rsa'
