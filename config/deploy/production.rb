ed94541bb24738c1018441fc108f5e66837e75de
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2017-01-19 05:16:13 +0900

    change config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '52.197.29.76', user: 'nozawa', roles: %w{app db web}
+server '52.199.125.13', user: 'ec2-user', roles: %w{app db web}
 
 #デプロイするサーバにsshログインする鍵の情報を記述
 set :ssh_options, keys: '~/.ssh/retrip_key_rsa'
