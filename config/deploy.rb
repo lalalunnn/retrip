0f822b9857253dd26b306a77ad8cae4f04705297
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2017-01-20 11:50:16 +0900

    revise capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/aws_pair.pem']
+                  keys: ['/Users/nozawa/.ssh/aws_key.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,1 @@
-# EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '52.199.125.13', user: 'ec2-user', roles: %w{app db web}
-
-#デプロイするサーバにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/retrip_key_rsa'
+server '13.112.48.155', user: 'ec2-user', roles: %w{app db web}
